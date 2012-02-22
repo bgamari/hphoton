04e6be8f92f60807096e46f62cd49261a822afef
Ben Gamari <bgamari.foss@gmail.com>
2012-02-21 21:53:57 -0500

    Alex: Remove unnecessary V.convert

--- HPhoton/FpgaTimetagger/Alex.hs
+++ HPhoton/FpgaTimetagger/Alex.hs
@@ -32,19 +32,20 @@ instance Functor Alex where
 -- | Extract timestamps for alternating laser excitation analysis
 alexTimes :: Time -> AlexChannels -> V.Vector Record -> Alex (V.Vector Time)
 alexTimes offset chs recs =
-        Alex { alexAexcAem = V.convert $ getTimes (achAexc chs) (achAem chs)
-             , alexDexcAem = V.convert $ getTimes (achDexc chs) (achAem chs)
-             , alexAexcDem = V.convert $ getTimes (achAexc chs) (achDem chs)
-             , alexDexcDem = V.convert $ getTimes (achDexc chs) (achDem chs) }
-        where f :: Channel -> Channel -> ([Time], Maybe Time) -> Record -> ([Time], Maybe Time)
-              f excCh _ (ts,_) (DeltaRecord {recChannels=newExcChs, recTime=t})
-                | excCh `elem` newExcChs        = (ts, Just (t+offset))
-                | otherwise                     = (ts, Nothing)
-              f _ emCh (ts, a@(Just startT)) (StrobeRecord {recChannels=ch, recTime=t})
-                | t > startT && emCh `elem` ch  = (t : ts, a)
-                | otherwise                     = (ts, a)
-              f _ _ (ts, Nothing) _             = (ts, Nothing)
-              initial = ([], Nothing)
-              getTimes excCh emCh = V.reverse $ V.fromList $ fst
-                                    $ V.foldl' (f excCh emCh) initial recs
+  Alex { alexAexcAem = getTimes (achAexc chs) (achAem chs)
+       , alexDexcAem = getTimes (achDexc chs) (achAem chs)
+       , alexAexcDem = getTimes (achAexc chs) (achDem chs)
+       , alexDexcDem = getTimes (achDexc chs) (achDem chs)
+       }
+  where f :: Channel -> Channel -> ([Time], Maybe Time) -> Record -> ([Time], Maybe Time)
+        f excCh _ (ts,_) (DeltaRecord {recChannels=newExcChs, recTime=t})
+          | excCh `elem` newExcChs        = (ts, Just (t+offset))
+          | otherwise                     = (ts, Nothing)
+        f _ emCh (ts, a@(Just startT)) (StrobeRecord {recChannels=ch, recTime=t})
+          | t > startT && emCh `elem` ch  = (t : ts, a)
+          | otherwise                     = (ts, a)
+        f _ _ (ts, Nothing) _             = (ts, Nothing)
+        initial = ([], Nothing)
+        getTimes excCh emCh = V.reverse $ V.fromList $ fst
+                              $ V.foldl' (f excCh emCh) initial recs
 
