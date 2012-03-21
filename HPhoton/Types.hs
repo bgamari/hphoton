b3cc5dda319b5563bbf68ee108e53101eaf3b0b2
Ben Gamari <bgamari.foss@gmail.com>
2012-03-21 14:21:33 -0400

    Add Timestamps type which may or may not get used

--- HPhoton/Types.hs
+++ HPhoton/Types.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell, TypeOperators #-}
+
 module HPhoton.Types ( -- * Time
                        Time
                      , TimeDelta
@@ -5,9 +7,15 @@ module HPhoton.Types ( -- * Time
                      , timeToRealTime
                      , realTimeToTime
                      , Span
+                       -- * Time stamps
+                     , Timestamps(..)
+                     , tsJiffy
+                     , duration, realDuration
                      ) where
 
+import Data.Label
 import Data.Word
+import qualified Data.Vector.Unboxed as V
 
 -- | A time in instrument-dependent ticks
 type Time = Word64
@@ -17,11 +25,34 @@ type TimeDelta = Word64
 -- | A real time given in seconds
 type RealTime = Double
 
+-- | Convert a Time to a RealTime
 timeToRealTime :: RealTime -> Time -> RealTime
 timeToRealTime jiffy t = jiffy * realToFrac t
 
+-- | Convert a RealTime to a Time
 realTimeToTime :: RealTime -> RealTime -> Time
 realTimeToTime jiffy rt = round $ rt / jiffy
 
 -- | A span of time given by `(start,end)`
 type Span = (Time, Time)
+
+-- | Represents a monotonic series of timestamps
+data Timestamps = TStamps { _tsFreq :: Word64 -- ^ Ticks per second
+                          , _tsStamps :: V.Vector Time -- ^ The timestamps in ticks
+                          }
+                  deriving (Show, Eq)
+$(mkLabels [''Timestamps])
+
+-- | Real time per tick
+tsJiffy :: Timestamps :-> RealTime
+tsJiffy = lens (\ts->1/realToFrac (get tsFreq ts))
+               (\jiffy->set tsFreq (round $ 1/jiffy))
+
+-- | The duration in ticks of a timestamp series
+duration :: Timestamps -> Time
+duration ts = V.last stamps - V.head stamps
+  where stamps = get tsStamps ts
+
+realDuration :: Timestamps -> RealTime
+realDuration ts = timeToRealTime (get tsJiffy ts) $ duration ts
+
