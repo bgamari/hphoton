0a0ee4599949d7e47cff9afcbc1f2f37bc309896
Ben Gamari <bgamari.foss@gmail.com>
2012-04-17 11:16:36 -0400

    Fret: Make Foldable

--- HPhoton/Fret/Types.hs
+++ HPhoton/Fret/Types.hs
@@ -5,6 +5,9 @@ module HPhoton.Fret.Types ( FretChannel(..)
                           , Gamma
                           ) where
 
+import Data.Foldable
+import Data.Monoid
+
 type FretEff = Double
 type ProxRatio = Double
 type Gamma = Double
@@ -16,3 +19,5 @@ data Fret a = Fret { fretA, fretD :: a } deriving (Show, Eq)
 instance Functor Fret where
   fmap f (Fret x y) = Fret (f x) (f y)
   
+instance Foldable Fret where
+  foldMap f (Fret x y) = f x <> f y
