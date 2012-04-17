616ceda3c8b79f6fd9fec5b1c9b0fbacf2086378
Ben Gamari <bgamari.foss@gmail.com>
2012-04-17 16:00:35 -0400

    BinThreshold: Rework handling of thresholding

--- HPhoton/BurstIdent/BinThreshold.hs
+++ HPhoton/BurstIdent/BinThreshold.hs
@@ -1,11 +1,26 @@
-module HPhoton.BurstIdent.BinThreshold where
+module HPhoton.BurstIdent.BinThreshold ( BinThreshold (..)
+                                       , findBursts
+                                       ) where
 
 import HPhoton.Types
 import HPhoton.Bin
 import qualified Data.Vector.Unboxed as V
+import Statistics.Distribution
+import Statistics.Distribution.Normal       
 
-findBursts :: TimeDelta -> Int -> V.Vector Time -> V.Vector Span
+-- | Bin thresholding criterion
+data BinThreshold = AbsThresh Int     -- ^ Absolute counts per bin
+                  | MeanThresh Double -- ^ Multiple of mean
+                  | VarThresh Double  -- ^ Multiple of variance above mean
+                  deriving (Show, Eq)
+
+findBursts :: TimeDelta -> BinThreshold -> V.Vector Time -> V.Vector Span
 findBursts width threshold ts =
-  let as = V.filter (\(t,n)->n > threshold)
-           $ binTimesWithTimes ts width
-  in V.map (\(t,n)->(t, t+width)) as
+  let bins = binTimesWithTimes ts width
+      countDist = normalFromSample $ V.map (realToFrac . snd) bins
+      thresh = case threshold of
+                    AbsThresh n  -> n
+                    MeanThresh x -> round $ x * mean countDist
+                    VarThresh x  -> round $ mean countDist + x * variance countDist
+  in V.map (\(t,n)->(t, t+width))
+     $ V.filter (\(t,n)->n > thresh) bins
