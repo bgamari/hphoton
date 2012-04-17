4e6a7acb1c6ef0c142c14fd5f690c44e7ff336c0
Ben Gamari <bgamari.foss@gmail.com>
2012-04-17 16:13:48 -0400

    BinThreshold: More work on thresholds

--- HPhoton/BurstIdent/BinThreshold.hs
+++ HPhoton/BurstIdent/BinThreshold.hs
@@ -5,22 +5,24 @@ module HPhoton.BurstIdent.BinThreshold ( BinThreshold (..)
 import HPhoton.Types
 import HPhoton.Bin
 import qualified Data.Vector.Unboxed as V
-import Statistics.Distribution
-import Statistics.Distribution.Normal       
+import Statistics.Sample
 
 -- | Bin thresholding criterion
-data BinThreshold = AbsThresh Int     -- ^ Absolute counts per bin
-                  | MeanThresh Double -- ^ Multiple of mean
-                  | VarThresh Double  -- ^ Multiple of variance above mean
+data BinThreshold = AbsThresh Int          -- ^ Absolute counts per bin
+                  | AbsMeanThresh Int      -- ^ Counts above mean
+                  | MultMeanThresh Double  -- ^ Multiple of mean
+                  | MultVarThresh Double   -- ^ Multiple of variance above mean
                   deriving (Show, Eq)
 
 findBursts :: TimeDelta -> BinThreshold -> V.Vector Time -> V.Vector Span
 findBursts width threshold ts =
   let bins = binTimesWithTimes ts width
-      countDist = normalFromSample $ V.map (realToFrac . snd) bins
+      counts = V.map (realToFrac . snd) bins
       thresh = case threshold of
-                    AbsThresh n  -> n
-                    MeanThresh x -> round $ x * mean countDist
-                    VarThresh x  -> round $ mean countDist + x * variance countDist
+                    AbsThresh n      -> n
+                    AbsMeanThresh n  -> round (mean counts) + n
+                    MultMeanThresh x -> round $ x * mean counts
+                    MultVarThresh x  -> let (m,s) = meanVariance counts
+                                        in round $ m + x * s
   in V.map (\(t,n)->(t, t+width))
      $ V.filter (\(t,n)->n > thresh) bins
