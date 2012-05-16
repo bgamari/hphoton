b91a24e2f1d6d82298ab6b3012ae1a27fe7686ef
Ben Gamari <bgamari.foss@gmail.com>
2012-05-16 12:59:17 -0400

    Work on Regions

--- HPhoton/Region.hs
+++ HPhoton/Region.hs
@@ -2,7 +2,8 @@
 
 module HPhoton.Region ( Region(..)
                       , elem
-                      , toUnions
+                      , toIntervals
+                      , fromIntervals
                       ) where
 
 import Prelude hiding (elem)       
@@ -17,24 +18,62 @@ data Region a = Interval (a,a)
               deriving (Show, Eq)
 
 elem :: Ord a => a -> Region a -> Bool
-a `elem` (Span (x,y))        = a >= x && a <= y
+a `elem` (Interval (x,y))    = a `elemI` (x,y)
 a `elem` (Union x y)         = a `elem` x || a `elem` y
 a `elem` (Diff x y)          = a `elem` x || not (a `elem` y)
 a `elem` (Intersection x y)  = a `elem` x && a `elem` y
 _ `elem` _                    = False
 
+elemI :: Ord a => a -> (a,a) -> Bool
+elemI x (a,b) = x >= a && x <= b
+
+subtractI :: Ord a => (a,a) -> (a,a) -> [(a,a)]
+subtractI s@(a,b) (c,d)
+    | (a,b) == (c,d)             = []
+    | c `elemI` s && d>b         = [(a,c)]
+    | c<a && d `elemI` s         = [(d,b)]
+    | c `elemI` s && d `elemI` s = [(a,c), (d,b)]
+    | otherwise                  = [(a,b)]
+
+intersectI :: Ord a => (a,a) -> (a,a) -> [(a,a)]
+intersectI s@(a,b) t@(c,d)
+    | (a,b) == (c,d)             = [(a,b)]                   
+    | c `elemI` s && d>b         = [(c,b)]
+    | c<a && d `elemI` s         = [(a,d)]
+    | c `elemI` s && d `elemI` s = [(c,d)]
+    | a `elemI` t && b `elemI` t = [(a,b)]
+    | otherwise                  = []
+
+fromIntervals :: Ord a => [(a,a)] -> Region a
+fromIntervals [x]      = Interval x              
+fromIntervals (x:rest) = Union (Interval x) (fromIntervals rest)
+
+toIntervals :: Ord a => Region a -> [(a,a)]
+toIntervals (Interval (a,b))   = [(a,b)]
+toIntervals (Union x y)        = toIntervals x ++ toIntervals y
+toIntervals (Diff x y)         = concat $ do (a,b) <- toIntervals x
+                                             (c,d) <- toIntervals y
+                                             return $ (a,b) `subtractI` (c,d)
+toIntervals (Intersection x y) = concat $ do (a,b) <- toIntervals x
+                                             (c,d) <- toIntervals y
+                                             return $ (a,b) `intersectI` (c,d)
+toIntervals Null               = []
+
 toUnions :: Ord a => Region a -> Region a
-toUnions (Diff (Span (a,b)) s@(Span (c,d)))
-    | a<c && b>d              = Union (Span (a,c)) (Span (d,b))
-    | a<c && b `elem` s       = Span (a,c)
-    | a `elem` s && b<d       = Span (d,b)
-    | otherwise               = Span (a,b)
-toUnions (Diff x y)           = let Union y1 y2 = toUnions y
-                                in toUnions $ Union (Diff x y1) (Diff x y2)
-toUnions (Intersection (Span (a,b)) (Span (c,d)))
-                              = Span (max a c, min b d)
+toUnions (Diff (Interval (a,b)) s@(Interval (c,d)))
+    | a<c && b>d              = Union (Interval (a,c)) (Interval (d,b))
+    | a<c && b `elem` s       = Interval (a,c)
+    | a `elem` s && b<d       = Interval (d,b)
+    | otherwise               = Interval (a,b)
+toUnions (Diff x y)           = let y' = toUnions y
+                                in toUnions $ Union (Diff x y') (Diff x y')
+toUnions (Intersection (Interval (a,b)) (Interval (c,d)))
+                              = Interval (max a c, min b d)
 toUnions (Intersection x y)   = let Union y1 y2 = toUnions y
                                 in toUnions $ Union (Intersection x y1) (Intersection x y2)
+toUnions (Union (Interval (x,y)) (Interval (x',y')))
+    | y == x'                 = Interval (x, y')
+toUnions (Union x y)          = Union (toUnions x) (toUnions y)
 toUnions a                    = a
          
 instance (Ord a) => Monoid (Sum (Region a)) where
