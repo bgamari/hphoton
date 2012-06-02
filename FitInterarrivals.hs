1ca963a652efadc57dc8a605309240f95c31c3a1
Ben Gamari <bgamari.foss@gmail.com>
2012-06-01 21:33:16 -0400

    FitInterarrivals: Large refactoring, run multiple chains in parallel

--- FitInterarrivals.hs
+++ FitInterarrivals.hs
@@ -1,31 +1,33 @@
 {-# LANGUAGE DeriveDataTypeable, TemplateHaskell #-}
 
-import System.IO
-import Data.Number.LogFloat hiding (realToFrac)
-import Data.Random.Lift
+import           System.IO
+import           Data.Number.LogFloat hiding (realToFrac)
+import           Data.Random.Lift
 import qualified Data.Vector as VB
-import qualified Data.Vector.Unboxed as V                
-import Control.Applicative                
-import Control.Monad                
-import           Data.Random                 
+import qualified Data.Vector.Unboxed as V
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Trans.Writer                 
+import           Data.Random
 import           Data.Random.Distribution.Beta
 import           Data.Random.Distribution.Categorical
-import System.Random.MWC (withSystemRandom)                 
+import           System.Random.MWC (withSystemRandom)
 
-import Numeric.MixtureModel.Exponential
+import           Numeric.MixtureModel.Exponential
 
-import Statistics.Sample (meanVariance)
-import Data.List (transpose)
+import           Statistics.Sample (meanVariance)
+import           Data.List (transpose)
 
+import           Control.Concurrent.ParallelIO
+import           Data.Accessor
+import           Data.Colour
+import           Data.Colour.Names
+import           Graphics.Rendering.Chart
+import           Graphics.Rendering.Chart.Plot.Histogram
 import           HPhoton.FpgaTimetagger
-import HPhoton.Utils
-import Data.Accessor       
-import Graphics.Rendering.Chart
-import Graphics.Rendering.Chart.Plot.Histogram       
-import Data.Colour
-import Data.Colour.Names       
-import Text.Printf       
-import System.Console.CmdArgs
+import           HPhoton.Utils
+import           System.Console.CmdArgs
+import           Text.Printf
 
 data FitArgs = FitArgs { chain_length     :: Int
                        , number_chains    :: Int
@@ -39,7 +41,7 @@ fitArgs = FitArgs { chain_length = 100 &= help "Length of Markov chain"
                   , number_chains = 200 &= help "Number of chains to run"
                   , sample_every = 5 &= help "Number of steps to skip between sampling parameters"
                   , burnin_length = 40 &= help "Number of steps to allow chain to burn-in for"
-                  , file = "" &= help "File to process" &= typFile &= argPos 0
+                  , file = "" &= typFile &= argPos 0
                   }
         &= summary "fit-interarrivals"
         &= details ["Fit interarrival times from mixture of Poisson processes"]
@@ -80,6 +82,17 @@ showStats v =
     let (mean,var) = meanVariance $ V.fromList v
     in printf "%1.3e    σ²=%1.3e"  mean var
 
+showChainStats :: Chain -> Writer String ()
+showChainStats chain = do
+    let components = transpose $ map VB.toList chain
+    forM_ (zip [1..] components) $ \(i,c)->do
+        let (w,p) = unzip c
+        tell $ "\nComponent "++show i++"\n"
+        tell $ "  Last Params: "++showExponential (last p)++"\n"
+        tell $ "  Weight:      "++showStats w++"\n"
+        tell $ "  <τ>:         "++showStats (map tauMean p)++"\n"
+        tell $ "  <τ²>:        "++showStats (map tauVariance p)++"\n"
+
 main = do
   fargs <- cmdArgs fitArgs
   recs <- readRecords $ file fargs
@@ -89,10 +102,31 @@ main = do
                 $ strobeTimes recs Ch0
              :: V.Vector Sample
 
-  withSystemRandom $ \mwc->do
-  assignments0 <- sampleFrom mwc $ updateAssignments samples initial
-  print $ paramsFromAssignments samples (VB.map snd initial) assignments0
+  chains <- parallelInterleaved $ map (runChain fargs samples) [1..number_chains fargs]
 
+  forM_ (zip [1..] chains) $ \(i,chain)->do
+      printf "\n\nChain %d\n" (i::Int)
+      putStr $ execWriter $ showChainStats chain
+  
+  putStr "\n\nChain totals\n"
+  putStr $ execWriter $ showChainStats $ concat chains
+  
+  let paramSample = last $ last chains -- TODO: Correctly average
+
+  let dist x = sum $ map (\(w,p)->w * realToFrac (prob p x)) $ VB.toList paramSample
+      layout = layout1_plots ^= [ Left $ histPlot samples
+                                , Left $ functionPlot 10000 (1e-7,longTime) dist
+                                ]
+             $ (layout1_left_axis .> laxis_generate) ^= autoScaledLogAxis defaultLogAxis
+             $ defaultLayout1
+  renderableToPDFFile (toRenderable layout) 640 480 "hi.pdf"
+
+-- | Parameter samples of a chain
+type Chain = [ComponentParams]
+
+runChain :: FitArgs -> V.Vector Sample -> Int -> IO Chain
+runChain fargs samples chainN = withSystemRandom $ \mwc->do
+  assignments0 <- sampleFrom mwc $ updateAssignments samples initial
   let f :: (ComponentParams, Assignments)
         -> RVarT IO ((ComponentParams, Assignments), ComponentParams)
       f (params, a) = do
@@ -100,28 +134,14 @@ main = do
         let params' = estimateWeights a'
                       $ paramsFromAssignments samples (VB.map snd params) a'
             l = likelihood samples params a'
-        lift $ hPutStr stderr $ printf "Likelihood: %1.5e\n" (logFromLogFloat l :: Double) 
+        lift $ hPutStr stderr $ printf "%d: Likelihood: %8f\n" chainN (logFromLogFloat l :: Double) 
         return ((params', a'), params')
   steps <- sampleFrom mwc $ replicateM' (chain_length fargs) f (initial, assignments0)
 
-  let paramSamples = transpose $ takeEvery (sample_every fargs)
-                     $ drop (burnin_length fargs)
-                     $ map VB.toList steps
-  forM_ (zip [0..] paramSamples) $ \(i,component)->do
-      let (w,p) = unzip component
-      putStrLn $ "\nComponent "++show i
-      putStrLn $ "  Last Params: "++showExponential (last p)
-      putStrLn $ "  Weight:      "++showStats w
-      putStrLn $ "  <τ>:         "++showStats (map tauMean p)
-      putStrLn $ "  <τ²>:        "++showStats (map tauVariance p)
-
-  let dist x = sum $ map (\(w,p)->w * realToFrac (prob p x)) $ VB.toList $ last steps
-      layout = layout1_plots ^= [ Left $ histPlot samples
-                                , Left $ functionPlot 10000 (1e-7,longTime) dist
-                                ]
-             $ (layout1_left_axis .> laxis_generate) ^= autoScaledLogAxis defaultLogAxis
-             $ defaultLayout1
-  renderableToPDFFile (toRenderable layout) 640 480 "hi.pdf"
+  let paramSamples :: Chain
+      paramSamples = takeEvery (sample_every fargs)
+                     $ drop (burnin_length fargs) steps
+  return paramSamples
 
 replicateM' :: Monad m => Int -> (a -> m (a,b)) -> a -> m [b]
 replicateM' n f a | n < 1 = error "Invalid count"
