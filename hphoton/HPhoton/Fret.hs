f5b2cc8cc4ff2eb692f2eb84df4949c84e7c49ef
Ben Gamari <bgamari.foss@gmail.com>
2012-08-22 18:37:59 -0400

    Fret: Implement unflipFrets

--- hphoton/HPhoton/Fret.hs
+++ hphoton/HPhoton/Fret.hs
@@ -1,12 +1,14 @@
 module HPhoton.Fret ( fretEfficiency
                     , proximityRatio
                     , gammaFromFret
-                    , flipFrets
+                    , flipFrets, unflipFrets
                     , module HPhoton.Fret.Types
                     ) where
 
-import HPhoton.Fret.Types
-  
+import           Control.Applicative (ZipList (..))
+import           Data.Traversable    (traverse)
+import           HPhoton.Fret.Types
+
 -- | `fretEfficiency gamma fret` is the gamma-corrected FRET efficiency
 -- for acceptor/donor intensities `fret`
 fretEfficiency :: Gamma -> Fret Double -> FretEff
@@ -16,14 +18,16 @@ fretEfficiency gamma x = fretA x / (fretA x + fretD x)
 -- intensities `fret`
 proximityRatio :: Fret Double -> ProxRatio
 proximityRatio = fretEfficiency 1
-          
+
 -- | `gammaFromFret proxRatio fretEff` is the gamma such that
 -- `proxRatio` is shifted to `fretEff`
 gammaFromFret :: ProxRatio -> FretEff -> Gamma
 gammaFromFret proxRatio fretEff = (1/fretEff - 1) / (1/proxRatio - 1)
 
 -- | Turn a 'Fret' of lists into a list of 'Fret's
--- TODO: Should/could this somehow be done with Traversable?
 flipFrets :: Fret [a] -> [Fret a]
-flipFrets (Fret a b) = zipWith Fret a b
+flipFrets (Fret a b) = getZipList $ traverse ZipList $ Fret a b
 
+-- | Turn a list of 'Fret's into a 'Fret' of lists
+unflipFrets :: [Fret a] -> Fret [a]
+unflipFrets xs = Fret (map fretA xs) (map fretD xs)
