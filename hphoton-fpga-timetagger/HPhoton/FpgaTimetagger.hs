af4339ced087a0b11ff3cf33708c9d6509cefff3
Ben Gamari <bgamari.foss@gmail.com>
2013-01-24 18:36:41 -0500

    FpgaTimetagger: Add documentation

--- hphoton-fpga-timetagger/HPhoton/FpgaTimetagger.hs
+++ hphoton-fpga-timetagger/HPhoton/FpgaTimetagger.hs
@@ -1,54 +1,54 @@
 {-# LANGUAGE BangPatterns, TypeFamilies, MultiParamTypeClasses, TemplateHaskell, RankNTypes #-}
 
-module HPhoton.FpgaTimetagger ( Channel(..)
-                                -- * Timetag Record
-                              , Record, emptyRecord
+module HPhoton.FpgaTimetagger ( -- * Types and fields
+                                Record, zeroRecord
                               , recDelta, recStrobe
                               , recTime
+                              , Channel(..)
                               , recChannel, recChannels
                               , recLost, recWrap
                                          
-                                -- * Reading
+                                -- * Reading records
                               , readRecords
+                              , decodeRecords
                               , strobeTimes
                                 
-                                -- * Utilities
-                              , getRecord
-       
+                                -- * Metadata
                               , module HPhoton.FpgaTimetagger.Metadata
                               ) where
 
-import Data.Maybe (catMaybes)
-import Data.Word
-import Data.Bits
-import Data.Bits.Lens
-import Control.Lens
-import Data.List (foldl')
-import HPhoton.Types
-import Foreign.Storable
-import Foreign.Ptr
+import           Data.Maybe (catMaybes)
+import           Data.Word
+import           Data.Bits
+import           Data.Bits.Lens
+import           Control.Lens
+import           Data.List (foldl')
+import           HPhoton.Types
+import           Foreign.Storable
+import           Foreign.Ptr
   
-import Data.Storable.Endian
-import qualified Data.Vector.Unboxed.Mutable as VM
-import Data.Vector.Unboxed.Base
-import Data.Vector.Unboxed.Deriving
-import qualified Data.Vector.Generic         as G
-import qualified Data.Vector.Generic.Mutable as M
+import           Control.Applicative ((<$>))
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Unsafe as BSU
+import           Data.Storable.Endian
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Generic.Mutable as M
+import           Data.Vector.Unboxed.Base
+import           Data.Vector.Unboxed.Deriving
+import qualified Data.Vector.Unboxed.Mutable as VM
 
-import Control.Monad (liftM, when)
-import Control.Monad.Trans.State
+import           Control.Monad (liftM, when)
+import           Control.Monad.Trans.State
 
-import HPhoton.FpgaTimetagger.Metadata
+import           HPhoton.FpgaTimetagger.Metadata
 
+-- | A timetagger input channel
 data Channel = Ch0 | Ch1 | Ch2 | Ch3 deriving (Show, Eq, Enum, Bounded)
 
+-- | A timetagger event record
 newtype Record = Record Word64 deriving (Eq)
 makeIso ''Record
         
-data RecType = Delta | Strobe deriving (Show, Eq, Enum, Bounded)
-                                              
 instance Show Record where
   show r = "mkRecord" 
              ++" "++views recType show r
@@ -57,14 +57,20 @@ instance Show Record where
              ++" "++views recWrap show r
              ++" "++views recLost show r
   
-emptyRecord = Record 0
+zeroRecord = Record 0
 
-recDelta, recStrobe, recWrap, recLost :: Lens' Record Bool
+-- | Is the record a delta record
+recDelta :: Lens' Record Bool
 recDelta = from record . bitAt 45
+
+-- | Is the record a strobe record
+recStrobe :: Lens' Record Bool
 recStrobe = recDelta . iso not not
-recWrap = from record . bitAt 46
-recLost = from record . bitAt 47
-        
+
+-- | The type of a record
+data RecType = Delta | Strobe deriving (Show, Eq, Enum, Bounded)
+                                              
+-- | The type of a record
 recType :: Lens' Record RecType
 recType = recDelta . iso to from
   where to True     = Delta
@@ -72,11 +78,21 @@ recType = recDelta . iso to from
         from Delta  = True
         from Strobe = False
               
+-- | The wrap bit of a record
+recWrap :: Lens' Record Bool
+recWrap = from record . bitAt 46
+
+-- | The "records lost" bit of a record
+recLost :: Lens' Record Bool
+recLost = from record . bitAt 47
+
+-- | The time field of a record
 recTime :: Lens' Record Time
 recTime = from record . lens (.&. 0xfffff)  set
   where set r t | t > 0xfffff = error "FpgaTimetagger: Time too large"
                 | otherwise   = (r .&. complement 0xfffff) .|. t
 
+-- | Is the given channel's bit set
 recChannel :: Channel -> Lens' Record Bool
 recChannel ch = from record . bitAt bit
     where bit = case ch of Ch0 -> 36
@@ -84,6 +100,7 @@ recChannel ch = from record . bitAt bit
                            Ch2 -> 38
                            Ch3 -> 39
 
+-- | All of the channel bits set in a record
 recChannels :: Record -> [Channel]
 recChannels r = filter (\ch->view (recChannel ch) r) $ enumFrom minBound
 
@@ -92,17 +109,23 @@ derivingUnbox "Record"
     [| \(Record r) -> fromIntegral r |]
     [| \r -> Record $ fromIntegral r |]
 
+-- | Read records from a file
+-- Note that this will drop the first 1024 records
+-- as a precaution against records leaking from stale buffers
 readRecords :: FilePath -> IO (Vector Record)
-readRecords fname = do
-    d <- BS.readFile fname
-    let f bs  | BS.length bs < 6  = Nothing
-              | otherwise         = case getRecord $ BS.take 6 bs of
-                                        Just r  -> Just (r, BS.drop 6 bs)
-                                        Nothing -> f (BS.drop 6 bs)
-    return $ G.drop 1024 $ G.unfoldr f d
+readRecords fname = G.drop 1024 . decodeRecords <$> BS.readFile fname
     
-getRecord :: BS.ByteString -> Maybe Record
-getRecord bs
+-- | Decode records from a bytestring
+decodeRecords :: BS.ByteString -> Vector Record
+decodeRecords = G.unfoldr f
+    where f bs | BS.length bs < 6  = Nothing
+               | otherwise         = case decodeRecord $ BS.take 6 bs of
+                                         Just r  -> Just (r, BS.drop 6 bs)
+                                         Nothing -> f (BS.drop 6 bs)
+    
+-- | Decode a single record from a bytestring
+decodeRecord :: BS.ByteString -> Maybe Record
+decodeRecord bs
     | BS.length bs < 6 = Nothing
     | otherwise        = Just $ Record
                          $ sum [ byte 0
@@ -125,9 +148,11 @@ unwrapTimes maxT ts = evalState (G.mapM f ts) (0,0)
                  put (offset', t)
                  return $! t + offset'
 
+-- | Return all of the strobe records for a given channel
 strobeRecords :: Vector Record -> Channel -> Vector Record
 strobeRecords recs ch = G.filter (\r->r^.recStrobe && r^.recChannel ch) recs
 
+-- | Return all of the strobe event times for a given channel
 strobeTimes :: Vector Record -> Channel -> Vector Time
 strobeTimes recs ch = G.map (view recTime) $ strobeRecords recs ch
 
