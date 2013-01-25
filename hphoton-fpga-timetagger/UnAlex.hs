c40435cce22d553e8348745b3310cd836d06732c
Ben Gamari <bgamari.foss@gmail.com>
2013-01-25 15:53:27 -0500

    UnAlex: Cleanup import list

--- hphoton-fpga-timetagger/UnAlex.hs
+++ hphoton-fpga-timetagger/UnAlex.hs
@@ -6,10 +6,9 @@ import           HPhoton.Types
 
 import           Control.Proxy as P
 import qualified Control.Proxy.ByteString as PBS
-import Control.Proxy.Trans.Writer
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
-import Control.Lens
+import           Control.Lens
 
 filterDeltas :: (Monad m, Proxy p) => () -> Pipe p Record Record m ()
 filterDeltas () = runIdentityP $ go Nothing where
