0b70d9bb6e778d3537b0bf8b5342ee09896f2bde
Ben Gamari <bgamari.foss@gmail.com>
2013-01-27 18:09:05 -0500

    Bin: Expose list forms of binning functions
    This allows me to consume bins lazily

--- hphoton/HPhoton/Bin.hs
+++ hphoton/HPhoton/Bin.hs
@@ -2,9 +2,13 @@
 
 module HPhoton.Bin ( -- * Temporal photon binning
                      bin
+                   , binL
                    , binWithBounds
+                   , binWithBoundsL
                    , binRange
+                   , binRangeL
                    , binRangeWithBounds
+                   , binRangeWithBoundsL
                    -- * Internal
                    , bin'
                    ) where
@@ -14,29 +18,42 @@ import qualified Data.Vector.Unboxed as V
 
 -- | 'bin width times' is a list of binned counts of bin width 'width'
 bin :: Time -> V.Vector Time -> V.Vector Int
-bin width ts =
-    V.fromList $ bin' width (V.toList ts) Nothing (fromIntegral $ V.head ts `quot` width) 0
+bin width ts = V.fromList $ binL width ts
+
+binL :: Time -> V.Vector Time -> [Int]
+binL width ts =
+    bin' width (V.toList ts) Nothing (fromIntegral $ V.head ts `quot` width) 0
                 
 -- | 'binWithBounds width times' is a list of bin counts and start
 -- times of bin width 'width'
 binWithBounds :: Time -> V.Vector Time -> V.Vector (Time, Int)
-binWithBounds width ts = V.zip times bins
+binWithBounds width ts = V.fromList $ binWithBoundsL width ts
+
+binWithBoundsL :: Time -> V.Vector Time -> [(Time, Int)]
+binWithBoundsL width ts = zip times bins
     where bin0 = fromIntegral $ V.head ts `quot` width
-          bins = V.fromList $ bin' width (V.toList ts) Nothing bin0 0
-          times = V.generate (V.length bins) (\i->fromIntegral i*width + V.head ts)
+          bins = bin' width (V.toList ts) Nothing bin0 0
+          times = map (\i->fromIntegral (i+bin0)*width) [0..]
 
 -- | 'binRange width (start,end) times' is a list of bin counts
 -- of bin width 'width'. The bins will begin at time 'start' and end
 -- at 'end'
 binRange :: Time -> (Time, Time) -> V.Vector Time -> V.Vector Int
-binRange width (start_t, end_t) ts =
-    V.fromList $ bin' width (V.toList ts) (Just end_t) (fromIntegral $ start_t `quot` width) 0
+binRange width (start_t, end_t) ts = V.fromList $ binRangeL width (start_t, end_t) ts
+
+binRangeL :: Time -> (Time, Time) -> V.Vector Time -> [Int]
+binRangeL width (start_t, end_t) ts =
+    bin' width (V.toList ts) (Just end_t) (fromIntegral $ start_t `quot` width) 0
 
 binRangeWithBounds :: Time -> (Time, Time) -> V.Vector Time -> V.Vector (Time, Int)
-binRangeWithBounds width (start_t, end_t) ts = V.zip times bins
+binRangeWithBounds width (start_t, end_t) ts =
+    V.fromList $ binRangeWithBoundsL width (start_t, end_t) ts
+
+binRangeWithBoundsL :: Time -> (Time, Time) -> V.Vector Time -> [(Time, Int)]
+binRangeWithBoundsL width (start_t, end_t) ts = zip times bins
     where bin0 = fromIntegral $ start_t `quot` width
-          bins = V.fromList $ bin' width (V.toList ts) (Just end_t) bin0 0
-          times = V.generate (V.length bins) (\i->fromIntegral i*width + start_t)
+          bins = bin' width (V.toList ts) (Just end_t) bin0 0
+          times = map (\i->fromIntegral (i+bin0)*width) [0..]
 
 bin' :: Time -> [Time] -> Maybe Time -> Int -> Int -> [Int]
 bin' width (t:ts) end_t !bin_n !count
