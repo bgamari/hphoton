ec3d26aa0bc0ca7d2256e83678f9f9252e8261b6
Ben Gamari <bgamari.foss@gmail.com>
2013-07-31 16:32:19 -0400

    HPhoton.Bin: Rename Vector.Unboxed import

--- hphoton/HPhoton/Bin.hs
+++ hphoton/HPhoton/Bin.hs
@@ -15,49 +15,49 @@ module HPhoton.Bin ( -- * Temporal photon binning
                    ) where
 
 import HPhoton.Types
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed as VU
 
 import           Control.Applicative
 import           Data.Foldable as F
 import           Data.Traversable as T
 
 -- | 'bin width times' is a list of binned counts of bin width 'width'
-bin :: Time -> V.Vector Time -> V.Vector Int
-bin width ts = V.fromList $ binL width ts
+bin :: Time -> VU.Vector Time -> VU.Vector Int
+bin width ts = VU.fromList $ binL width ts
 
-binL :: Time -> V.Vector Time -> [Int]
+binL :: Time -> VU.Vector Time -> [Int]
 binL width ts =
-    bin' width (V.toList ts) Nothing (fromIntegral $ V.head ts `quot` width) 0
+    bin' width (VU.toList ts) Nothing (fromIntegral $ VU.head ts `quot` width) 0
 
 -- | 'binWithBounds width times' is a list of bin counts and start
 -- times of bin width 'width'
-binWithBounds :: Time -> V.Vector Time -> V.Vector (Time, Int)
-binWithBounds width ts = V.fromList $ binWithBoundsL width ts
+binWithBounds :: Time -> VU.Vector Time -> VU.Vector (Time, Int)
+binWithBounds width ts = VU.fromList $ binWithBoundsL width ts
 
-binWithBoundsL :: Time -> V.Vector Time -> [(Time, Int)]
+binWithBoundsL :: Time -> VU.Vector Time -> [(Time, Int)]
 binWithBoundsL width ts = zip times bins
-    where bin0 = fromIntegral $ V.head ts `quot` width
-          bins = bin' width (V.toList ts) Nothing bin0 0
+    where bin0 = fromIntegral $ VU.head ts `quot` width
+          bins = bin' width (VU.toList ts) Nothing bin0 0
           times = map (\i->fromIntegral (i+bin0)*width) [0..]
 
 -- | 'binRange width (start,end) times' is a list of bin counts
 -- of bin width 'width'. The bins will begin at time 'start' and end
 -- at 'end'
-binRange :: Time -> (Time, Time) -> V.Vector Time -> V.Vector Int
-binRange width (start_t, end_t) ts = V.fromList $ binRangeL width (start_t, end_t) ts
+binRange :: Time -> (Time, Time) -> VU.Vector Time -> VU.Vector Int
+binRange width (start_t, end_t) ts = VU.fromList $ binRangeL width (start_t, end_t) ts
 
-binRangeL :: Time -> (Time, Time) -> V.Vector Time -> [Int]
+binRangeL :: Time -> (Time, Time) -> VU.Vector Time -> [Int]
 binRangeL width (start_t, end_t) ts =
-    bin' width (V.toList ts) (Just end_t) (fromIntegral $ start_t `quot` width) 0
+    bin' width (VU.toList ts) (Just end_t) (fromIntegral $ start_t `quot` width) 0
 
-binRangeWithBounds :: Time -> (Time, Time) -> V.Vector Time -> V.Vector (Time, Int)
+binRangeWithBounds :: Time -> (Time, Time) -> VU.Vector Time -> VU.Vector (Time, Int)
 binRangeWithBounds width (start_t, end_t) ts =
-    V.fromList $ binRangeWithBoundsL width (start_t, end_t) ts
+    VU.fromList $ binRangeWithBoundsL width (start_t, end_t) ts
 
-binRangeWithBoundsL :: Time -> (Time, Time) -> V.Vector Time -> [(Time, Int)]
+binRangeWithBoundsL :: Time -> (Time, Time) -> VU.Vector Time -> [(Time, Int)]
 binRangeWithBoundsL width (start_t, end_t) ts = zip times bins
     where bin0 = fromIntegral $ start_t `quot` width
-          bins = bin' width (V.toList ts) (Just end_t) bin0 0
+          bins = bin' width (VU.toList ts) (Just end_t) bin0 0
           times = map (\i->fromIntegral (i+bin0)*width) [0..]
 
 bin' :: Time -> [Time] -> Maybe Time -> Int -> Int -> [Int]
@@ -83,8 +83,8 @@ bin' _ [] Nothing _ _                    = []
 
 -- | Bin simultaneous photon streams. Here we take the bin range to be
 -- the intersection of all of the streams.
-binMany :: (Traversable f, Applicative f) => Time -> f (V.Vector Time) -> [f Int]
+binMany :: (Traversable f, Applicative f) => Time -> f (VU.Vector Time) -> [f Int]
 binMany binWidth times =
     getZipList $ T.sequenceA $ pure (ZipList . binRangeL binWidth (start,end)) <*> times
-    where start = F.maximum $ fmap V.head times
-          end   = F.minimum $ fmap V.last times
+    where start = F.maximum $ fmap VU.head times
+          end   = F.minimum $ fmap VU.last times
