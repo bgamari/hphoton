aae9068058615fad7362623816e5429ac4ac04f9
Ben Gamari <bgamari.foss@gmail.com>
2013-08-01 13:57:26 -0400

    BayesBurstFind: Fix style

--- hphoton-tools/BayesBurstFind.hs
+++ hphoton-tools/BayesBurstFind.hs
@@ -36,41 +36,42 @@ burstFind = BurstFind
               <> help "Acceptance threshold on the Bayes factor" )
 
 main :: IO ()
-main = do let opts = info (helper <*> burstFind)
-                          ( fullDesc <> progDesc "Bayesian fluorescence burst identification" )
-          args <- execParser opts
-          let realRateToTau rate = round $ realToFrac (clockrate args) / rate
-              mp = ModelParams { mpWindow = burst_length args
-                               , mpProbB = 0.05
-                               , mpTauBg = realRateToTau $ bg_rate args
-                               , mpTauBurst = realRateToTau $ burst_rate args
-                               }
+main = do
+    let opts = info (helper <*> burstFind)
+                    ( fullDesc <> progDesc "Bayesian fluorescence burst identification" )
+    args <- execParser opts
+    let realRateToTau rate = round $ realToFrac (clockrate args) / rate
+        mp = ModelParams { mpWindow = burst_length args
+                         , mpProbB = 0.05
+                         , mpTauBg = realRateToTau $ bg_rate args
+                         , mpTauBurst = realRateToTau $ burst_rate args
+                         }
 
-          d <- readRecords (fname args)
-          let clk = clockFromFreq $ clockrate args
-          let fret = Fret { fretA = strobeTimes d Ch0, fretD = strobeTimes d Ch1 }
-              times = combineChannels [fretA fret, fretD fret]
+    d <- readRecords (fname args)
+    let clk = clockFromFreq $ clockrate args
+    let fret = Fret { fretA = strobeTimes d Ch0, fretD = strobeTimes d Ch1 }
+        times = combineChannels [fretA fret, fretD fret]
 
-          let dts = timesToInterarrivals times
-              duration = (jiffy clk * fromIntegral (V.last times - V.head times))
-          printf "%d photons\n" (V.length times)
-          printf "Timestamp range %u..%u : %4.2e seconds\n" (V.head times) (V.last times) duration
-          printf "Average rate %1.3f photons/second\n" $ (fromIntegral $ V.length dts) / duration
-          print mp
+    let dts = timesToInterarrivals times
+        duration = (jiffy clk * fromIntegral (V.last times - V.head times))
+    printf "%d photons\n" (V.length times)
+    printf "Timestamp range %u..%u : %4.2e seconds\n" (V.head times) (V.last times) duration
+    printf "Average rate %1.3f photons/second\n" $ (fromIntegral $ V.length dts) / duration
+    print mp
 
-          let burstTimes = V.map (times!) $ findBurstPhotons mp (beta_thresh args) dts
-              nBurst = V.length burstTimes
-          if nBurst == 0
-             then putStrLn "No bursts found"
-             else do printf "Found %u burst photons\n" nBurst
-                     let cspans = V.toList $ compressSpans (40*mpTauBurst mp) burstTimes
-                         counts = fmap (map V.length . spansPhotons cspans) fret
-                     printf "Average %f photons/burst\n"
-                       (realToFrac nBurst / realToFrac (length cspans) :: Double)
-       
-                     let printSpan (start,end) counts =
-                           printf "%9u\t%9u\t%4u\t%4u" start end (fretA counts) (fretD counts)
-                     writeFile (fname args) $ unlines
-                       $ zipWith printSpan cspans (flipFrets counts)
+    let burstTimes = V.map (times!) $ findBurstPhotons mp (beta_thresh args) dts
+        nBurst = V.length burstTimes
+    if nBurst == 0
+       then putStrLn "No bursts found"
+       else do printf "Found %u burst photons\n" nBurst
+               let cspans = V.toList $ compressSpans (40*mpTauBurst mp) burstTimes
+                   counts = fmap (map V.length . spansPhotons cspans) fret
+               printf "Average %f photons/burst\n"
+                 (realToFrac nBurst / realToFrac (length cspans) :: Double)
+    
+               let printSpan (start,end) counts =
+                     printf "%9u\t%9u\t%4u\t%4u" start end (fretA counts) (fretD counts)
+               writeFile (fname args) $ unlines
+                 $ zipWith printSpan cspans (flipFrets counts)
                      
 
