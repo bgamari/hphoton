7547ea837366c0e65f834302709332b49ad120ca
Ben Gamari <bgamari.foss@gmail.com>
2013-08-12 13:28:45 -0400

    FpgaTimetagger: Port to pipes-4.0

--- hphoton-fpga-timetagger/HPhoton/FpgaTimetagger/Pipe.hs
+++ hphoton-fpga-timetagger/HPhoton/FpgaTimetagger/Pipe.hs
@@ -1,40 +1,36 @@
 module HPhoton.FpgaTimetagger.Pipe ( decodeRecordsP
-                                   --, readRecordsP
                                    , filterDeltasP
                                    , encodeRecordsP
                                    , module HPhoton.FpgaTimetagger
                                    ) where
 
 import           Control.Lens
-import           Control.Proxy as P
-import qualified Control.Proxy.ByteString as PBS
+import           Pipes
+import qualified Pipes.Prelude as P
+import qualified Pipes.ByteString as PBS
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Unsafe as BU
 import           HPhoton.FpgaTimetagger
 
 -- | Decode records
-decodeRecordsP :: (Monad m, Proxy p) => () -> Pipe p BS.ByteString Record m ()
-decodeRecordsP = P.runIdentityK (go BS.empty) where
+decodeRecordsP :: (Monad m) => Pipe BS.ByteString Record m r
+decodeRecordsP = go BS.empty where
     go bs
-        | BS.length bs < 6 = \x -> do bs' <- P.request x
-                                      go (bs `BS.append` bs') x
-        | otherwise = \x -> do case decodeRecord $ BU.unsafeTake 6 bs of
-                                   Just r  -> P.respond r >>= go (BU.unsafeDrop 6 bs)
-                                   Nothing -> go (BU.unsafeDrop 6 bs) x
-    
--- | Read records from a file
---readRecordsP :: (Monad m, Proxy p) => FilePath -> () -> Producer p Record m ()
---readRecordsP fname = PBS.fromHandleS fname >-> decodeRecordsP
+        | BS.length bs < 6 = do bs' <- await
+                                go (bs `BS.append` bs')
+        | otherwise = do case decodeRecord $ BU.unsafeTake 6 bs of
+                             Just r  -> yield r >> go (BU.unsafeDrop 6 bs)
+                             Nothing -> go (BU.unsafeDrop 6 bs)
 
-encodeRecordsP :: (Monad m, Proxy p) => () -> Pipe p Record BS.ByteString m ()
-encodeRecordsP = mapD encodeRecord
+encodeRecordsP :: (Monad m) => Pipe Record BS.ByteString m r
+encodeRecordsP = P.map encodeRecord
 
 -- | Drop delta records that have no strobe events after them               
-filterDeltasP :: (Monad m, Proxy p) => () -> Pipe p Record Record m ()
-filterDeltasP () = runIdentityP $ go Nothing where
-    go lastDelta = do r <- P.request ()
+filterDeltasP :: (Monad m) => Pipe Record Record m r
+filterDeltasP = go Nothing where
+    go lastDelta = do r <- await
                       if r^.recDelta
                           then go (Just r)
-                          else do maybe (return ()) P.respond lastDelta
-                                  P.respond r
+                          else do maybe (return ()) yield lastDelta
+                                  yield r
                                   go Nothing
