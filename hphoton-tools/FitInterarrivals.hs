0f0c74b33a63e04951bbf2a001ce4fbc66477393
Ben Gamari <bgamari.foss@gmail.com>
2013-09-17 13:03:01 -0400

    FitInterarrivals: Use optparse-applicative

--- hphoton-tools/FitInterarrivals.hs
+++ hphoton-tools/FitInterarrivals.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DeriveDataTypeable, TemplateHaskell #-}
-
 import           System.IO
 import           Numeric.Log hiding (sum, Exp)
 import           Data.Random.Lift
@@ -24,7 +22,7 @@ import           Control.Concurrent.ParallelIO
 import           HPhoton.FpgaTimetagger
 import           HPhoton.Utils
 import           HPhoton.Types hiding (Freq)
-import           System.Console.CmdArgs
+import           Options.Applicative
 import           Text.Printf
 
 type Freq = Double
@@ -42,100 +40,119 @@ data FitArgs = FitArgs { chain_length     :: Int
                        , short_cutoff     :: RealTime
                        , verbose          :: Bool
                        }
-             deriving (Data, Typeable, Show)
-       
-fitArgs = FitArgs
-    { file = "" &= typFile &= argPos 0
-    , clockrate = 128e6 &= groupname "Input"
-                        &= typ "FREQ"
-                        &= help "Instrument clockrate (default=128 MHz)"
-    , channel = 0 &= groupname "Input"
-                  &= help "Channel to fit"
-    , short_cutoff = 1e-6 &= typ "TIME"
-                          &= groupname "Input"
-                          &= help "Discard interarrival times smaller than TIME (default=1 us)"
-    , model = Nothing &= typFile
-                      &= groupname "Model"
-                      &= help "Model file"
+             deriving (Show)
 
-    , all_chains = False &= groupname "Output"
-                         &= help "Show statistics from all chains"
-    , output = "" &= groupname "Output"
-                  &= help "Output model to file"
-    , verbose = False &= groupname "Output"
-                      &= help "Display status updates during chain run"
-    
-    , chain_length = 100 &= help "Length of Markov chain (default=100)"
-                         &= name "l"
-                         &= groupname "Sampler"
-    , number_chains = 8 &= help "Number of chains to run (default=8)"
-                        &= groupname "Sampler"
-    , sample_every = 5 &= help "Number of steps to skip between sampling parameters (default=5)"
-                       &= groupname "Sampler"
-    , burnin_length = 40 &= help "Number of steps to allow chain to burn-in for (default=40)"
-                         &= groupname "Sampler"
-    }
-    &= program "fit-interarrivals"
-    &= summary "Fit interarrival times to a mixture of exponential distributions."
-    &= details [ "The fitter utilizes a Gibbs sampling approach and can be configured"
-               , "to fit either standard or power exponential distributions to photon"
-               , "arrival time data. For the time being, the program requires that "
-               , "this data be in Goldner lab's FPGA timetagger record format."
-               , ""
-               , ""
-               , " Models"
-               , "==========="
-               , ""
-               , "By default, the program will fit to a two-component exponential model"
-               , "with,"
-               , ""
-               , "  * a low-rate (λ=50 initially) exponential component modelling"
-               , "    background"
-               , ""
-               , "  * a high-rate (λ=5000 initially) stretched exponential"
-               , "    (β=1 initially) component modelling fluorescence from diffusing"
-               , "    fluorophores"
-               , ""
-               , "All parameters (λ in the case of a standard exponential and λ and β"
-               , "in the stretched case) in addition to component weighing factors"
-               , "are allowed to vary during the fit."
-               , ""
-               , "While this has been found to work well for donor-only FRET data,"
-               , "any arbitrary model can be specified with the use of a model file"
-               , "(given by the --model option). This is a standard text file listing"
-               , "the model components and initial parameters. For instance, the"
-               , "default model is given by,"
-               , ""
-               , "  [ (0.7, Exp 50)"
-               , "  , (0.3, StretchedExp 5000 1)"
-               , "  ]"
-               , ""
-               , ""
-               , " Sampler"
-               , "============"
-               , ""
-               , "By default, the program will evaluate 8 sampler chains of 100 samples"
-               , "each to provide robust estimates of distribution parameters and"
-               , "variances on these quantities. If the quality of the produced variance"
-               , "estimates are less useful, one can reduce the number of chains with "
-               , "the --number-chains option. It is recommended that the chain length "
-               , "and burn-in time are not reduced to ensure convergence."
-               , ""
-               , "Because fits involving many chains can be time-consuming, it recommended"
-               , "that the calculation be parallelized. This can be accomplished by"
-               , "passing the '+RTS -N n -RTS' argument on the command line, replacing"
-               , "'n' with the number of cores to use."
-               , ""
-               , ""
-               , " Afterpulsing correction"
-               , "============================"
-               , "Often data from single-photon sensitive detectors will be afflicted"
-               , "with non-Poissonian afterpulsing events. These artifacts can severely"
-               , "bias a fit if left uncorrected. The easiest way to avoid this is to"
-               , "cut off short inter-arrival time events by setting the --short-cutoff"
-               , "option. The default of 1 microsecond appears to be a reasonable setting"
-               , "for our detectors."
-               ]
+fitArgs :: Parser FitArgs
+fitArgs = FitArgs
+    <$> option  ( long "chain-length" <> short 'l'
+               <> metavar "N" <> value 100
+               <> help "Length of Markov chain"
+                )
+    <*> option  ( long "num-chains" <> short 'n'
+               <> metavar "N" <> value 8
+               <> help "Number of chains to run"
+                )
+    <*> option  ( long "sample-every" <> short 's'
+               <> metavar "N" <> value 5
+               <> help "Number of steps to skip between sampling parameters (default=5)"
+                )
+    <*> option  ( long "burnin-length" <> short 'b'
+               <> metavar "N" <> value 40
+               <> help "Number of steps to allow chain to burn-in for (default=40)"
+                )
+    <*> switch  ( long "all-chains" <> short 'a'
+               <> help "Show statistics from all chains"
+                )
+    <*> option  ( long "model" <> short 'm'
+               <> metavar "MODEL" <> value Nothing
+               <> reader (fmap Just . auto)
+               <> help "Model file"
+                )
+    <*> argument pure ( metavar "FILE" <> help "Input file" )
+    <*> option  ( long "output" <> short 'o'
+               <> metavar "FILE" <> help "Output model to file"
+                )
+    <*> option  ( long "channel" <> short 'c'
+               <> value 0 <> metavar "N"
+               <> help "Channel to fit"
+                )
+    <*> option  ( long "clockrate"
+               <> metavar "FREQ" <> value 128e6
+               <> help "Instrument clockrate (default=128 MHz)"
+                )
+    <*> option  ( long "short-cutoff"
+               <> metavar "TIME" <> value 1e-6
+               <> help "Discard interarrival times smaller than TIME"
+                )
+    <*> switch  ( long "verbose" <> short 'v'
+               <> help "Display status updates during chain run"
+                )
+
+parserInfo = info fitArgs
+    $ fullDesc
+   <> header "fit-interarrivals - Fit interarrival times to a mixture of exponential distributions."
+   <> progDesc helpMsg
+
+helpMsg = unlines
+    [ "The fitter utilizes a Gibbs sampling approach and can be configured"
+    , "to fit either standard or power exponential distributions to photon"
+    , "arrival time data. For the time being, the program requires that "
+    , "this data be in Goldner lab's FPGA timetagger record format."
+    , ""
+    , ""
+    , " Models"
+    , "==========="
+    , ""
+    , "By default, the program will fit to a two-component exponential model"
+    , "with,"
+    , ""
+    , "  * a low-rate (λ=50 initially) exponential component modelling"
+    , "    background"
+    , ""
+    , "  * a high-rate (λ=5000 initially) stretched exponential"
+    , "    (β=1 initially) component modelling fluorescence from diffusing"
+    , "    fluorophores"
+    , ""
+    , "All parameters (λ in the case of a standard exponential and λ and β"
+    , "in the stretched case) in addition to component weighing factors"
+    , "are allowed to vary during the fit."
+    , ""
+    , "While this has been found to work well for donor-only FRET data,"
+    , "any arbitrary model can be specified with the use of a model file"
+    , "(given by the --model option). This is a standard text file listing"
+    , "the model components and initial parameters. For instance, the"
+    , "default model is given by,"
+    , ""
+    , "  [ (0.7, Exp 50)"
+    , "  , (0.3, StretchedExp 5000 1)"
+    , "  ]"
+    , ""
+    , ""
+    , " Sampler"
+    , "============"
+    , ""
+    , "By default, the program will evaluate 8 sampler chains of 100 samples"
+    , "each to provide robust estimates of distribution parameters and"
+    , "variances on these quantities. If the quality of the produced variance"
+    , "estimates are less useful, one can reduce the number of chains with "
+    , "the --number-chains option. It is recommended that the chain length "
+    , "and burn-in time are not reduced to ensure convergence."
+    , ""
+    , "Because fits involving many chains can be time-consuming, it recommended"
+    , "that the calculation be parallelized. This can be accomplished by"
+    , "passing the '+RTS -N n -RTS' argument on the command line, replacing"
+    , "'n' with the number of cores to use."
+    , ""
+    , ""
+    , " Afterpulsing correction"
+    , "============================"
+    , "Often data from single-photon sensitive detectors will be afflicted"
+    , "with non-Poissonian afterpulsing events. These artifacts can severely"
+    , "bias a fit if left uncorrected. The easiest way to avoid this is to"
+    , "cut off short inter-arrival time events by setting the --short-cutoff"
+    , "option. The default of 1 microsecond appears to be a reasonable setting"
+    , "for our detectors."
+    ]
 
 argsChannel :: FitArgs -> Channel
 argsChannel (FitArgs {channel=ch}) =
@@ -204,7 +221,7 @@ statusWorker chains = do
         else statusWorker chains
 
 main = do
-  fargs <- cmdArgs fitArgs
+  fargs <- execParser parserInfo
   recs <- readRecords $ file fargs
   let jiffy = 1 / clockrate fargs
       samples = V.filter (>short_cutoff fargs)
