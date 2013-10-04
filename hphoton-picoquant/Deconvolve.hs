53917a487f019e6fe677d5dac6d898fafa1609f0
Ben Gamari <bgamari.foss@gmail.com>
2013-10-04 14:49:22 -0400

    Work on Deconvole

--- hphoton-picoquant/Deconvolve.hs
+++ hphoton-picoquant/Deconvolve.hs
@@ -1,27 +1,33 @@
 {-# LANGUAGE ScopedTypeVariables, BangPatterns #-}
+{-# LANGUAGE DataKinds, RankNTypes #-}
                 
 module Deconvolve where
 
+import Data.Distributive
 import Data.Traversable as T
 import Data.Foldable as F
 import Control.Applicative
 import Linear
+import Linear.V
+
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
 
 -- | @richardsonLucy psf x@ is the deconvolution of @x@
 -- under point spread function @psf@.
 richardsonLucy :: forall f g a.
-                ( Functor f, Applicative f, Traversable f, Metric f
-                , Functor g, Applicative g, Metric g
-                , RealFrac a)
+                ( Functor f, Applicative f, Metric f
+                , Functor g, Applicative g, Metric g, Distributive g
+                , Fractional a, Conjugate a)
                => f (g a) -> f a -> [g a]
 richardsonLucy p d = richardsonLucy' p d (pure 0.5)
 
 -- | @richardsonLucy' psf x u0@ is the deconvolution of @x@
 -- under point spread function @psf@ with initial guess @u0@.
 richardsonLucy' :: forall f g a.
-                 ( Functor f, Applicative f, Traversable f, Metric f
-                 , Functor g, Applicative g, Metric g
-                 , RealFrac a)
+                 ( Functor f, Applicative f, Metric f
+                 , Functor g, Applicative g, Metric g, Distributive g
+                 , Fractional a, Conjugate a)
                 => f (g a) -> f a -> g a -> [g a]
 richardsonLucy' p d = go
   where go :: g a -> [g a]
@@ -32,6 +38,14 @@ richardsonLucy' p d = go
                 f :: f a -> a -> a
                 f pj uj = uj * (d_c `dot` pj)
                 u' :: g a
-                !u' = f <$> T.sequenceA p <*> u
+                !u' = f <$> adjoint p <*> u
                
+data V' a = V' {runV' :: forall k. Dim k => V k a}
 
+richardsonLucyV :: (Fractional a, Conjugate a)
+                => V.Vector (V.Vector a) -> V.Vector a -> [V.Vector a]
+richardsonLucyV p d =
+    map toVector $ richardsonLucy (vector $ fmap vector p) (vector d)
+  where vector :: forall k a. Dim k => V.Vector a -> V k a
+        vector v = case fromVector v of Just v' -> v'
+  
--- hphoton-picoquant/Test.hs
+++ hphoton-picoquant/Test.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, DataKinds, ExistentialQuantification #-}
+
 import Deconvolve
 import Data.Csv as Csv
 import Data.Char
@@ -7,31 +9,51 @@ import qualified Data.ByteString.Lazy as LBS
 import Control.Lens       
 import           Data.Vector (Vector)
 import qualified Data.Vector as V
+import Linear       
+import Linear.V
+{-
 import Graphics.Rendering.Chart.Gtk
 import Graphics.Rendering.Chart
 import Data.Colour
 import qualified Data.Colour.Names as C
+-}
 
-readCorr :: FilePath -> IO (Either String (V.Vector (Int, Double)))
+data Pair a b = Pair !a !b
+              deriving (Show, Read, Eq, Ord)
+
+instance Field1 (Pair a b) (Pair a' b) a a' where
+    _1 k (Pair a b) = indexed k (0::Int) a <&> \a'-> Pair a' b
+instance Field2 (Pair a b) (Pair a b') b b' where
+    _2 k (Pair a b) = indexed k (0::Int) b <&> \b'-> Pair a b'
+    
+readCorr :: FilePath -> IO (Either String (V.Vector (Int,Double)))
 readCorr fname =
     Csv.decodeWith decodeOpts False <$> LBS.readFile fname
   where decodeOpts = Csv.defaultDecodeOptions { Csv.decDelimiter = fromIntegral $ ord '\t' }
 
-adjustCorr :: V.Vector (Int, Double) -> V.Vector (Int, Double)
+adjustCorr :: V.Vector (Int,Double) -> V.Vector (Pair Int Double)
 adjustCorr xs = V.take 3000
-              $ V.map (\(x,y)->(x-x0, y)) xs
+              $ V.map (\(x,y)->(Pair (x-x0) y)) xs
     where (x0,_) = V.head xs
 
 shiftPsf :: V.Vector a -> V.Vector (V.Vector a)
 shiftPsf v = V.generate (V.length v) shift
   where shift i = V.drop i v V.++ V.take i v
 
+vectorPlease :: forall k a. Dim k => Vector a -> V k a
+vectorPlease v = case fromVector v of Just v' -> v'
+
 main = do
     Right irf <- fmap adjustCorr <$> readCorr "water.txt"
     Right d <- fmap adjustCorr <$> readCorr "frnad.txt"
     
-    let deconv = head $ drop 10
-                 $ richardsonLucy (shiftPsf $ V.map snd irf) (V.map snd d)
+    let d' = vectorPlease $ V.map (view _2) d :: Dim k => V k Double
+        irf' = fmap vectorPlease $ shiftPsf $ V.map (view _2) irf
+             :: (Dim k1) => Vector (V k1 Double)
+        deconv = head $ drop 10
+                 $ richardsonLucy (vectorPlease irf') d'
+    print deconv
+    {-
     let plot d = Left $ toPlot $ plot_points_values .~ V.toList d $ def
         plotPlusses color d =
             Left $ toPlot
@@ -47,4 +69,4 @@ main = do
                    ]
                $ def
     renderableToWindow (toRenderable layout) 640 480
-    
+    -}
