8f0491d8c0a918fb84be2b6cbc52c21728dc3ccb
Ben Gamari <bgamari.foss@gmail.com>
2014-09-22 16:54:51 -0400

    fpga-timetagger: Add Pipes-based unwrapping

--- hphoton-fpga-timetagger/HPhoton/IO/FpgaTimetagger/Pipes.hs
+++ hphoton-fpga-timetagger/HPhoton/IO/FpgaTimetagger/Pipes.hs
@@ -2,16 +2,25 @@ module HPhoton.IO.FpgaTimetagger.Pipes
     ( decodeRecords
     , filterDeltas
     , encodeRecords
+    , unwrapRecords
+    , Photon
     , module Fpga
     ) where
 
+import Control.Monad.Trans.State
+
 import           Control.Lens
+
 import           Pipes
 import qualified Pipes.Prelude as PP
 import qualified Pipes.ByteString as PBS
+
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Unsafe as BU
+
 import           HPhoton.IO.FpgaTimetagger as Fpga hiding (decodeRecords)
+import           HPhoton.Types (Time)
+import qualified HPhoton.Unwrap as Unwrap
 
 -- | Decode records
 decodeRecords :: (Monad m) => Pipe BS.ByteString Record m r
@@ -38,3 +47,15 @@ filterDeltas = go Nothing where
                                   yield r
                                   go Nothing
 {-# INLINEABLE filterDeltas #-}
+
+type Photon = (Channel, Time)
+
+unwrapRecords :: Monad m => Pipe Record Photon m r
+unwrapRecords = go Unwrap.initialUnwrapState
+  where
+    go s = do
+      r <- await
+      let (t, s') = runState (Unwrap.unwrapTime recTimeMask (r ^. recTime)) s
+      mapM_ (\ch->yield (ch, t)) (recChannels r)
+      go s'
+{-# INLINEABLE unwrapRecords #-}
