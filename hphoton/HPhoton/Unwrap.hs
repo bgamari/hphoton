31be97f73a824c67c9b739b8e0bf097cb7f4ded5
Ben Gamari <bgamari.foss@gmail.com>
2014-09-22 16:32:55 -0400

    Make timestamp unwrapping code more reusable

--- hphoton/HPhoton/Unwrap.hs
+++ hphoton/HPhoton/Unwrap.hs
@@ -1,4 +1,9 @@
-module HPhoton.Unwrap (unwrapTimes) where
+module HPhoton.Unwrap ( unwrapTimes
+                        -- * Monadic building blocks
+                      , UnwrapState
+                      , initialUnwrapState
+                      , unwrapTime
+                      ) where
 
 import           Control.Monad.Trans.State
 import qualified Data.Vector.Generic         as G
@@ -7,14 +12,24 @@ import           HPhoton.Types
 data UnwrapState a = US { offset :: !a
                         , lastT  :: !a
                         }
-                      
+
+unwrapTime :: (Ord a, Num a, Monad m)
+           => a -> a -> StateT (UnwrapState a) m a
+unwrapTime maxT t = do
+    US offset lastT <- get
+    let offset' = if t < lastT
+                     then offset+maxT
+                     else offset
+    put $! US offset' t
+    return $! t + offset'
+{-# INLINABLE unwrapTime #-}
+
+initialUnwrapState :: Num a => UnwrapState a
+initialUnwrapState = US 0 0
+{-# INLINABLE initialUnwrapState #-}
+
 -- | Fix timing wraparounds
 unwrapTimes :: (Ord a, Num a, G.Vector v a) => a -> v a -> v a
-unwrapTimes maxT ts = evalState (G.mapM f ts) (US 0 0)
-  where f t = do US offset lastT <- get
-                 let offset' = if t < lastT
-                                   then offset+maxT
-                                   else offset
-                 put $ US offset' t
-                 return $! t + offset'
+unwrapTimes maxT ts =
+    evalState (G.mapM (unwrapTime maxT) ts) initialUnwrapState
 {-# INLINABLE unwrapTimes #-}
