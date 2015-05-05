c749b5e288ddcd70146788b3700abcd995f938b9
Ben Gamari <ben@smart-cactus.org>
2015-05-04 23:15:56 -0400

    PackedVec: Add dotSqr properties

--- hphoton-correlate/tests/HPhoton/Corr/PackedVec/Tests.hs
+++ hphoton-correlate/tests/HPhoton/Corr/PackedVec/Tests.hs
@@ -25,6 +25,13 @@ prop_dot_shift_invar x y shift =
         dot2 = dot (shiftVec shift x) (shiftVec shift y)
     in counterexample ("initial="++show dot1++" final="++show dot2) (dot1==dot2)
 
+prop_dotSqr_shift_invar :: (Num i, Ord i, Num a, Eq a, V.Vector v (i,a), Show a)
+                        => PackedVec v i a -> PackedVec v i a -> i -> Property
+prop_dotSqr_shift_invar x y shift =
+    let dot1 = PV.dotSqr x y
+        dot2 = PV.dotSqr (shiftVec shift x) (shiftVec shift y)
+    in counterexample ("initial="++show dot1++" final="++show dot2) (dot1==dot2)
+
 instance (Num i, Ord i, Arbitrary i, Arbitrary a, V.Vector v (i,a)) =>
          Arbitrary (PackedVec v i a) where
     arbitrary = do dxs <- arbitrary
@@ -66,6 +73,14 @@ prop_dense_dot_mag xs ys =
         mag2 = V.sum $ V.zipWith (*) xs ys
     in counterexample ("initial="++show mag1++" final="++show mag2) (mag1==mag2)
 
+prop_dense_dotSqr_mag :: (V.Vector v a, V.Vector v (Int,a), Num a, Eq a, Show a)
+                      => v a -> v a -> Property
+prop_dense_dotSqr_mag xs ys =
+    let mag1 = dotSqr (unsafePackedVec $ V.indexed xs) (unsafePackedVec $ V.indexed ys)
+        ss2 = V.sum $ V.zipWith (*) (V.map (^2) xs) (V.map (^2) ys)
+        dot2 = V.sum $ V.zipWith (*) xs ys
+    in counterexample ("initial="++show mag1++" final="++show (dot2,ss2)) (mag1 == (dot2,ss2))
+
 instance (VU.Unbox a, Arbitrary a) => Arbitrary (VU.Vector a) where
     arbitrary = sized $ \n->V.replicateM n arbitrary
 
@@ -77,7 +92,11 @@ tests =
     [ testProperty "Dot product magnitude" (withPV prop_dot_mag)
     , testProperty "Dense dot product magnitude"
                    (prop_dense_dot_mag :: VU.Vector Int -> VU.Vector Int -> Property)
+    , testProperty "Dense dot, squared dot product magnitude"
+                   (prop_dense_dotSqr_mag :: VU.Vector Int -> VU.Vector Int -> Property)
     , testProperty "Dot product shift invariant" (withPV prop_dot_shift_invar)
+    , testProperty "Dot, squared dot product shift invariant"
+      (withPV prop_dotSqr_shift_invar)
     , testProperty "Shift/unshift is identity" (withPV prop_shift_unshift_invar)
     , testProperty "dropWhileIdx works" (withPV prop_dropWhileIdx)
     , testProperty "takeWhileIdx works" (withPV prop_takeWhileIdx)
