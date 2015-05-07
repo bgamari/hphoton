e8a6ba10ea72e30819cda7f6b97bffb5d59a5064
Ben Gamari <ben@smart-cactus.org>
2015-05-06 23:10:56 -0400

    FpgaTimetagger: Fix style

--- hphoton-fpga-timetagger/HPhoton/IO/FpgaTimetagger/Pipes.hs
+++ hphoton-fpga-timetagger/HPhoton/IO/FpgaTimetagger/Pipes.hs
@@ -28,16 +28,17 @@ decodeRecords = go BS.empty where
     go bs
         | BS.length bs < 6 = do bs' <- await
                                 go (bs `BS.append` bs')
-        | otherwise = do case Fpga.decodeRecord $ BU.unsafeTake 6 bs of
-                             Just r  -> yield r >> go (BU.unsafeDrop 6 bs)
-                             Nothing -> go (BU.unsafeDrop 6 bs)
+        | otherwise =
+          case Fpga.decodeRecord $ BU.unsafeTake 6 bs of
+           Just r  -> yield r >> go (BU.unsafeDrop 6 bs)
+           Nothing -> go (BU.unsafeDrop 6 bs)
 {-# INLINEABLE decodeRecords #-}
 
 encodeRecords :: (Monad m) => Pipe Record BS.ByteString m r
 encodeRecords = PP.map Fpga.encodeRecord
 {-# INLINEABLE encodeRecords #-}
 
--- | Drop delta records that have no strobe events after them               
+-- | Drop delta records that have no strobe events after them
 filterDeltas :: (Monad m) => Pipe Record Record m r
 filterDeltas = go Nothing where
     go lastDelta = do r <- await
